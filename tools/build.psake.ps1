be6cf19e3bab32315d85b15525389a3b820db688
Jonathan Medd <jon_medd@hotmail.com>
2021-05-18 12:58:53 +0100

    test

--- tools/build.psake.ps1
+++ tools/build.psake.ps1
@@ -2,3 +2,20 @@
 
 Write-Host "Pester Module version is:"
 Get-Module -Name Pester
+
+# --- Dot source build.settings.ps1
+# . $PSScriptRoot\build.settings.ps1
+
+# --- Add any parameters from build.ps1
+properties {
+}
+
+# --- Define the build tasks
+# Task Default -depends Build
+# Task Build -depends Lint, UpdateModuleManifest, CreateArtifact, CreateArchive
+# Task BuildWithTests -depends Init, Build, ExecuteTest, UpdateDocumentation
+
+Task BuildWithTests {
+
+    Write-Host "Hello there"
+}
